fefc0529f807c5e7e31597a26e4df4d8dbaf7302
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-02-16 23:58:36 +0500

    update

--- loops.py
+++ loops.py
@@ -56,4 +56,6 @@ for i in range(5, 0, -1):
 my_dict = {"apple": 1, "banana": 2, "cherry": 3}
 for key, value in my_dict.items():
     print(f"{key}: {value}")
-    
+
+print("All the loops are executed successfully")
+print("All is done")
